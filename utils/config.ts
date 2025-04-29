06691cb476c597c3b80a7d97d47e91fdaf63c029
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-04-29 17:16:06 -0300

    utils/config was added for setting enviroment

---
+++ utils/config.ts
@@ -0,0 +1,9 @@
+export const ENV = 'qa'
+
+export const CONFIG ={
+    development: 'https://demoqa.com',
+    qa:'https://demoqa.com',
+    production:'https://demoqa.com',
+}
+
+export const BASE_URL = CONFIG[ENV];
