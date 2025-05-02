3133f52bb1cf4709e036e39dd5dbc110177d90a5
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-05-02 17:36:14 -0300

    BronkenLink tests were added

---
+++ tests/elements/BrokenLinkTest.spec.ts
@@ -0,0 +1,43 @@
+import{test,expect} from '@playwright/test'
+import { ElementPage } from '../../pages/elementPage';
+
+test.describe('broken link test', async () => {
+    let elementPage: ElementPage;
+
+    test.beforeEach(async ({ page }) => {
+        elementPage = new ElementPage(page);
+        await elementPage.goto();
+        await elementPage.brokenLinksImagesOption.click();
+    });
+
+    test('valid image should to have valid dimensions', async () => {
+        const dimensions = await elementPage.validImageLink.evaluate((img) => {
+            const image = img as HTMLImageElement;
+            return { width: image.naturalWidth, height: image.naturalHeight };
+        });
+        expect(dimensions.width).toBeGreaterThan(0);
+        expect(dimensions.height).toBeGreaterThan(0);
+    });
+
+    test('valid image should return 200 status', async ({ page }) => {
+        const src = await elementPage.validImageLink.getAttribute('src');
+        const response = await page.goto('https://demoqa.com/broken/images/Toolsqa.jpg');
+        expect(response?.status()).toBe(200);
+    });
+
+    test('image should to be visible', async () => {
+        await expect (elementPage.validImageLink).toBeVisible();
+    })
+
+    test('broken image should not to work', async () => {
+        const naturalWidth = await elementPage.brokenImageLink.evaluate((img) =>{
+            const image =img as HTMLImageElement;
+            return image.naturalWidth
+        });
+        console.log(naturalWidth);
+        expect(naturalWidth).toBe(0);
+    })
+
+
+
+});
