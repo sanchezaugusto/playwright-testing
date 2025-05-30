7ec6096a16f13fcce68d1522aca0bf5d6c06e1e5
sanchezaugusto <sanchez.augusto.n@gmail.com>
2025-05-29 21:16:13 -0300

    CheckBoxTest was reformed

--- tests/elements/CheckBoxTest.spec.ts
+++ tests/elements/CheckBoxTest.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect} from '@playwright/test';
-import { ElementPage } from '../../pages/elementPage';
+import { ElementPage } from '../../pages/ElementPage';
 
 test.describe('elements test', async () => {
     
